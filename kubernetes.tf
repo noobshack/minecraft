25357a94a8bd13c9d1217deb4620ed3e70d648be
Michael Simo <os@mpmsimo.com>
2019-06-08 13:41:49 -0700

    Added kubernetes manifest

---
+++ kubernetes.tf
@@ -0,0 +1,76 @@
+resource "kubernetes_deployment" "minecraft" {
+  metadata {
+    name = local.service
+
+    labels = {
+      app = local.service
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = local.service
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = local.service
+        }
+      }
+
+      spec {
+        host_network = true
+
+        container {
+          image = "gcr.io/${local.project}/minecraft:${local.minecraft_version}"
+          name  = local.service
+
+          resources {
+            limits {
+              cpu    = "1"
+              memory = "6Gi"
+            }
+
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "minecraft" {
+  metadata {
+    name = local.service
+
+    labels = {
+      app = local.service
+    }
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.minecraft.metadata[0].labels.app
+    }
+
+    session_affinity = "ClientIP"
+
+    type = "LoadBalancer"
+
+    port {
+      name        = "gameserver"
+      port        = 25565
+      target_port = 25565
+    }
+
+  }
+}
+
